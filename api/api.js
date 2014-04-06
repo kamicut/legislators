5d25ee91843cd514b2eaf417aefb897c6a5c5f2a
Marc Farra <marcfarra@gmail.com>
2014-04-06 05:05:44 +0300

    Major rewrite of API

---
+++ api/api.js
@@ -0,0 +1,46 @@
+var restify = require('restify');
+var routes  = require('./routes.js');
+
+var server = restify.createServer({name: 'legislators-api'});
+
+server
+.use(restify.fullResponse())
+.use(restify.queryParser({ mapParams: false }));
+
+
+function dataHandler(res, next) {
+	'use strict';
+	return function(err, data) {
+			if (err) {
+				return next(err);
+			}
+			res.send(data);
+			return next();
+		};
+}
+
+function list(routeName) {
+	'use strict';
+	return function(req, res, next) {
+		res.charSet('utf-8');
+		routes[routeName](dataHandler(res, next));
+	};
+}
+
+//Routes
+server.get('/api/search', function(req, res, next) {
+	'use strict';
+	res.charSet('utf-8');
+	if (req.query && Object.keys(req.query).length > 0) {
+		routes.search(req.query, dataHandler(res, next));
+	} else {
+		return next(new restify.InvalidArgumentError('Params must be supplied'));
+	}
+});
+
+server.get('/api/districts', list('districts'));
+server.get('/api/names', list('names'));
+server.get('/api/parties', list('parties'));
+
+server.listen(8080);
+
---
+++ api/routes.js
@@ -0,0 +1,110 @@
+var csv = require('csv-stream');
+var request = require('request');
+
+module.exports = (function() {
+	'use strict';
+	var baseUrl = 'https://docs.google.com/spreadsheet/tq?' +
+	'key=0Aqv3NjQVGHDbdGZNTXFiWlg4ZFFSdDhiSXU1d25MU1E&pub=1&tqx=out:csv';
+
+	var categoryDict = {
+		'first_name': 'A',
+		'first_name_ar': 'B',
+		'last_name': 'C',
+		'last_name_ar': 'D',
+		'gender': 'E',
+		'gender_ar': 'F',
+		'district': 'K',
+		'party': 'O',
+		'party_ar': 'P',
+		'sect': 'Q',
+		'born_day': 'R',
+		'born_month': 'S',
+		'born_year': 'T',
+		'other_notes': 'V'
+	};
+	var booleanDict = {
+		'email': 'J',
+		'phone': 'G',
+		'fax': 'H',
+		'mobile': 'I',
+		'facebook': 'L',
+		'twitter': 'M',
+		'website': 'U',
+	};
+	var setDict = {
+		'deputies_terms': 'N',
+	};
+
+	function buildResponse(paramString, callback) {
+		var res = [];
+		var options = {enclosedChar: '"'};
+		var csvStream = csv.createStream(options);
+		var sane = true;
+		request(baseUrl + paramString)
+		.pipe(csvStream)
+		.on('data', function(data) {
+			for (var k in data) {
+				sane = sane && (k !== 'undefined');
+			}
+			res.push(data);
+		})
+		.on('end', function() {
+			if (sane) {
+				callback(null, res);
+			} else {
+				callback(new Error('Some or all of your parameters could not be processed.'), null);
+			}
+		});
+	}
+	return {
+
+		search:function (params, callback) {
+			var paramString = '&tq=where%20';
+			var numParams = 0;
+
+			//Create the Query 
+			for (var k in params) {
+				var kv = '';
+				if (k in categoryDict) {
+					kv = categoryDict[k] + '=%27' + params[k] +'%27%20';
+				}
+				if (k in booleanDict && params[k] === 'true') {
+					kv = booleanDict[k] +'!=%27%27%20';
+				}
+				if (k in setDict) {
+					kv = setDict[k]+'%20contains%20%27' + params[k] + '%27%20';
+				}
+				paramString += (numParams > 0)? 'and%20' + kv: kv;
+				numParams += 1;
+			}
+
+			//Create the JSON Object
+			if (numParams == 0) {
+				callback()
+			}
+			buildResponse(paramString, callback);
+		},
+
+		districts: function(callback) {
+			var paramString = '&tq=select%20'+categoryDict.district +
+				',count('+categoryDict.first_name+')%20group%20by%20'+
+				categoryDict.district + '%20label%20count('+categoryDict.first_name+')%20' +
+				'%27legislators%27';
+			buildResponse(paramString, callback);
+		},
+		parties: function(callback) {
+			var paramString = '&tq=select%20'+categoryDict.party + ',' + categoryDict.party_ar + 
+				',count('+categoryDict.first_name+')%20group%20by%20'+
+				categoryDict.party +',' + categoryDict.party_ar +
+				'%20label%20count('+categoryDict.first_name+')%20' +
+				'%27legislators%27';
+			buildResponse(paramString, callback);
+		},
+		names: function(callback) {
+			var paramString = '&tq=select%20'+categoryDict.first_name + ',' +
+			categoryDict.last_name + ',' + categoryDict.first_name_ar + ',' +
+			categoryDict.last_name_ar;
+			buildResponse(paramString, callback);
+		},
+	};
+})();
